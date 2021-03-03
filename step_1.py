c29ccbd27583d40ea702cde436842b154c806645
AntonShishkin <nuegotonevzlomaut@gmail.com>
2021-03-03 23:22:03 +0300

    Create 1 question

---
+++ step_1.py
@@ -0,0 +1,6 @@
+name_1 = input("Введите имя: ")
+surname_1 = input("Введите фамилию: ")
+var_1 = int(input("Введите номер варианта: "))
+quest = int(input("Введите номер задания: "))
+answ = int(input("Введите ответ: "))
+print(f"{name_1} {surname_1} выполнил вариант {var_1}. В задании {quest} он получил ответ {answ}.")
